37638a62a9511431e99ab5e59b539a116bb7b14a
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-28 15:01:16 +0700

    Modify install.sh to update proxychains4.conf
    Update proxychains4 configuration for socks5 settings.

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ sudo apt-get install wget screen tar -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
 chmod +x mine.sh
+sudo sed -i 's/^socks4.*/#&/' /etc/proxychains4.conf
+sudo sed -i 's/^socks5.*/#&/' /etc/proxychains4.conf
+echo "socks5 95.164.245.175 9864 tykQ7b qxmTUk" | sudo tee -a /etc/proxychains4.conf
+sudo sed -i 's/^proxy_dns/#proxy_dns/' /etc/proxychains4.conf
 sudo ./mine.sh
