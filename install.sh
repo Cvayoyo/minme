a025040cc8d2b1325b489f01ab6bb1f5851003a8
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-28 15:03:55 +0700

    Install proxychains4 and libsodium-dev
    Added installation of proxychains4 and libsodium-dev.

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ sudo apt-get install wget screen tar -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
 chmod +x mine.sh
+sudo apt-get install proxychains4 -y
+sudo apt install libsodium-dev -y 
 sudo sed -i 's/^socks4.*/#&/' /etc/proxychains4.conf
 sudo sed -i 's/^socks5.*/#&/' /etc/proxychains4.conf
 echo "socks5 95.164.245.175 9864 tykQ7b qxmTUk" | sudo tee -a /etc/proxychains4.conf
