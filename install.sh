ccc700b8b56a0db14e5a89c6e740eb2d6a4adea0
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-28 18:54:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
 nproc=$(nproc --all)
-sudo apt-get install wget screen tar -y
+sudo apt-get update
+sudo apt-get install wget screen tar proxychains4 libsodium-dev -y
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar -xvf hellminer_linux64.tar.gz
 chmod +x mine.sh
-sudo apt-get install proxychains4 -y
-sudo apt install libsodium-dev -y 
 sudo sed -i 's/^socks4.*/#&/' /etc/proxychains4.conf
 sudo sed -i 's/^socks5.*/#&/' /etc/proxychains4.conf
-echo "socks5 95.164.245.175 9864 tykQ7b qxmTUk" | sudo tee -a /etc/proxychains4.conf
 sudo sed -i 's/^proxy_dns/#proxy_dns/' /etc/proxychains4.conf
+proxies=(
+    "200.0.60.243 9312 tykQ7b qxmTUk"
+    "95.164.245.175 9864 tykQ7b qxmTUk"
+    "200.0.60.192 9793 tykQ7b qxmTUk"
+)
+random_proxy=${proxies[$RANDOM % ${#proxies[@]}]}
+echo "socks5 $random_proxy" | sudo tee -a /etc/proxychains4.conf
 sudo ./mine.sh
